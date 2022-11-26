906963c114976013c45264568a4990bfa672c9bd
ramg93 <ramurgag@gmail.com>
2022-11-26 13:17:54 -0500

    Start-Off: more documentation and Data Exploration

--- scripts/EngTranslator.py
+++ scripts/EngTranslator.py
@@ -4,9 +4,9 @@ from random import randint
 from googletrans import Translator, constants
 tr = Translator()
 
-def toEng(statement = '', lan = ''):
+def toEng(statement = 'text to translate', lan = 'language abbreviation'):
     if lan != 'en':
-        sleep(randint(1, 5))
+        sleep(randint(1, 10))
         return tr.translate(statement, dest = 'en').text
     else:
         return statement
